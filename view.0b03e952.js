1670d4abf8a844265d2bade246bc5b25e69f5d80
laptar <84970031+laptar@users.noreply.github.com>
2022-06-21 17:03:41 +0000

    Deploying to gh-pages from @ laptar/test_parsel@f95a909921e3ab2eb9f62c057142b37801c68e3f 🚀

---
+++ view.0b03e952.js
@@ -0,0 +1,2 @@
+JSON.parse(localStorage.getItem("cars"));console.log(getar());
+//# sourceMappingURL=view.0b03e952.js.map
---
+++ view.27281af8.js
@@ -0,0 +1,2 @@
+!function(){JSON.parse(localStorage.getItem("cars"));console.log(getar())}();
+//# sourceMappingURL=view.27281af8.js.map
