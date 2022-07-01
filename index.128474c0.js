08b0cd07546cf15734f39de7c6a82ab353ecfb61
laptar <84970031+laptar@users.noreply.github.com>
2022-07-01 15:41:01 +0000

    Deploying to gh-pages from @ laptar/test_parsel@f452d950eaf22b2588af748ce916993f0080c46b 🚀

--- index.128474c0.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".api-search").addEventListener("input",(function(e){console.log(e.target.value);(t=e.target.value,void fetch(`https://swapi.dev/api/people/?search=${t}`).then((e=>e.json())).then((e=>console.log(e)))).then((e=>console.log(e)));var t}));
-//# sourceMappingURL=index.128474c0.js.map
